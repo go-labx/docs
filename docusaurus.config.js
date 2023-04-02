67b32dffdf13685a7362610fdbee8678e51a25f9
wanggan.kk <wanggan.kk@bytedance.com>
2023-04-02 18:18:28 +0800

    chore: update pkg

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'go-labx', // Usually your GitHub org/user name.
+  organizationName: 'lightning', // Usually your GitHub org/user name.
   projectName: 'lightning.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
